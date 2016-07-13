b05229ccddd634443deb9d58fa6ec7731e679106
bosconero <angelonastasi@hotmail.com>
2016-07-13 23:17:34 +0200

    Add files via upload

--- www/js/imbarcamento.js
+++ www/js/imbarcamento.js
@@ -1,21 +1,55 @@
+var imbares;
 function controllo() {
     "use strict";
     if ( document.getElementById('rck').value === '') {
         var attenzione = document.getElementById('attenzione');
-        attenzione.innerHTML = "<span style='background-color:#222;font:bold 16px arial;color:#fff;line-height:22px;display:block;margin:0 auto;'>Devi inserire la R<sub>ck</sub></span>";
+        alert("Devi inserire la Rck");
     document.getElementById('rck').focus();
-    } else {controllo1();}
+    } else {controllo5();}
+}
+function controllo5(){
+var cespo=document.getElementById('classesp').value;
+var rck=document.getElementById('rck').value;
+if(cespo=='XC1' && rck < 30){
+alert("ATTENZIONE:"+"\n"+"\n"+ " Rck inferiore a 30, minima classe di resistenza ammessa per XC1");
+document.getElementById('rck').value='';
+document.getElementById('rck').focus();
+
+}else if (cespo=='XC2' && rck < 30){
+alert("ATTENZIONE:"+"\n"+"\n"+ " Rck inferiore a 30, minima classe di resistenza ammessa per XC2");
+document.getElementById('rck').value='';
+document.getElementById('rck').focus();
+}else if (cespo=='XC3' && rck < 35) {
+alert("ATTENZIONE: "+"\n"+"\n"+ "Rck inferiore a 35, minima classe di resistenza ammessa per XC3");
+document.getElementById('rck').value='';
+document.getElementById('rck').focus();
+}else if(cespo=='XC4' && rck < 40){
+alert("ATTENZIONE:"+"\n"+"\n"+ " Rck inferiore a 40, minima classe di resistenza ammessa per XC4");
+document.getElementById('rck').value='';
+document.getElementById('rck').focus();
+}else if(cespo=='XF3' && rck < 30){
+alert("ATTENZIONE:"+"\n"+"\n"+ " Rck inferiore a 30, minima classe di resistenza ammessa per XF3");
+document.getElementById('rck').value='';
+document.getElementById('rck').focus();
+}else if (cespo=='XF4' && rck < 35){
+alert("ATTENZIONE:"+"\n"+"\n"+ " Rck inferiore a 35, minima classe di resistenza ammessa per XF4");
+document.getElementById('rck').value='';
+document.getElementById('rck').focus();
+} 
+else {controllo1()
+
+}
 }
 function controllo1() {
     "use strict"
      if ( document.getElementById('digiucont').value === '') {
-         attenzione.innerHTML="<span style='background-color:#222;font:bold 16px arial;color:#fff;line-height:22px;display:block;margin:0 auto;'>Devi inserire la distanza dei giunti di contrazione</span>" ;
+         alert("Devi inserire la distanza dei giunti di contrazione") ;
          document.getElementById('digiucont').focus();
          } else{controllo2();}
 }
 function controllo2(){
 if(document.getElementById('umidita').value==''){
-attenzione.innerHTML="<span style='background-color:#222;font:bold 16px arial;color:#fff;line-height:22px;display:block;margin:0 auto;'>Devi inserire l'umidit&agrave; relativa</span>" ;
+alert("Devi inserire l'umidit&agrave; relativa") ;
 document.getElementById('umidita').focus();
 } else{
 controllo3();}
@@ -23,7 +57,7 @@ controllo3();}
 
 function controllo3(){
 if(document.getElementById('spesspav').value==''){
-attenzione.innerHTML="<span style='background-color:#222;font:bold 16px arial;color:#fff;line-height:22px;display:block;margin:0 auto;'>Devi inserire lo spessore del pavimento </span>" ;
+alert("Devi inserire lo spessore del pavimento") ;
 document.getElementById('spesspav').focus();
 } else{
 imbarcamento();}
@@ -45,18 +79,30 @@ if ( document.getElementById('barriera').checked) {
 } else {
 barvap="no";
 }
-if (tgetto=="diretto" && cespo==0.6){
+if (tgetto=="diretto" && cespo=='XC1'){
+var srit=700;}
+if (tgetto=="diretto" && cespo=='XC2'){
 srit=700;}
-if (tgetto=="diretto" && cespo==0.55){
+if (tgetto=="diretto" && cespo=='XC3'){
 srit=650;}
-if (tgetto=="diretto" && cespo==0.5){
+if (tgetto=="diretto" && cespo=='XC4'){
 srit=600;}
-if (tgetto=="pompa" && cespo==0.6){
+if (tgetto=="diretto" && cespo=='XF3'){
+srit=600;}
+if (tgetto=="diretto" && cespo=='XF4'){
+srit=550;}
+if (tgetto=="pompa" && cespo=='XC1'){
 srit=800;}
-if (tgetto=="pompa" && cespo==0.55){
+if (tgetto=="pompa" && cespo=='XC2'){
+srit=800;}
+if (tgetto=="pompa" && cespo=='XC3'){
 srit=750;}
-if (tgetto=="pompa" && cespo==0.5){
+if (tgetto=="pompa" && cespo=='XC4'){
 srit=700;}
+if (tgetto=="pompa" && cespo=='XF3'){
+srit=700;}
+if (tgetto=="pompa" && cespo=='XF4'){
+srit=650;}
 if(umrel<40){
 umi=1.15;
 }else if (umrel==40) {
@@ -68,7 +114,12 @@ umi=0.9-((umrel-60)*0.02);
 }else if(umrel>80){
 umi=0.45;
 }
-
+if(cespo=='XC1'){ var ac=0.6; }
+if(cespo=='XC2'){ var ac=0.6; }
+if(cespo=='XC3'){ var ac=0.55; }
+if(cespo=='XC4'){ var ac=0.5; }
+if(cespo=='XF3'){ var ac=0.5; }
+if(cespo=='XF4'){ var ac=0.45; }
 var ritteo=(umi*srit*1.15)/1000000;
 var disdia=Math.sqrt((Math.pow(distcont,2)+Math.pow(distcont,2))) ;
 var raggio=((spess*(1-ritteo))/ritteo)/100;
@@ -87,9 +138,11 @@ var imbavapo = Math.round((imbarcafinale+(imbarcafinale*15/100))*100)/100;
 imbarcamentot.innerHTML="nel caso il pavimento sia su un supporto impermeabile o foglio di polietilene  l'imbarcamento tra il centro e l'incrocio dei giunti sar&agrave; di mm <span style='font:bold 20px arial;color:#ff2200;'>"+imbavapo+"  </span>";
 }
 if (barvap=="no"){
-    var imbares = imbarcamfinale;
+    imbares = imbarcamfinale;
+ 
     } else if(barvap=="si"){
     imbares = imbavapo;
+    
     }
 var alert=document.getElementById('alertimbarca')  ;
 if (imbares<2.5){
@@ -120,7 +173,7 @@ document.getElementById('intro').style.display="none";
 document.getElementById('pinserimento').style.display="block";
 }
 function esci(){
- navigator.app.exitApp();
+ app.exitApp();
 }
 function ripeti(){
 window.location="index.html";
@@ -129,6 +182,23 @@ function gocalcolo(){
  window.location = "imbarcamento.html";
 
 } 
+function inviamail(){
+var pac=document.getElementById('classesp').value;
+var contr=document.getElementById('digiucont').value;
+var getto=document.getElementById('tipogetto').value;
+var cls=document.getElementById('rck').value;
+var spessore=document.getElementById('spesspav').value;
+if(document.getElementById('barriera').checked){
+var strimp="si";
+}else{
+var strimp="no";
+}
+
+
+var mess="DATI DI CALCOLO %0d%0a -------------------------%0d%0a%0d%0a Classe esposizione cls: "+pac+"%0d%0aRck calcestruzzo        : "+cls+" N/mmq%0d%0aSpessore pavimento   : "+spessore+" cm%0d%0aDistanza giunti contr: "+contr+" ml%0d%0aTipo di getto             : "+getto+"%0d%0aSottofondo impermeabile :"+strimp+"%0d%0a%0d%0a%0d%0aRISULTATI%0d%0a----------------------%0d%0aL'imbarcamento atteso della piastra sar&agrave; di "+ imbares +" mm.;%0d%0a" ;
+var oggetto="Calcolo imbarcamento pavimento cantiere di"  ;
+document.location.href = "mailto:?"+"Subject=" + oggetto + "&Body=" + mess; 
+}
 
 
 
